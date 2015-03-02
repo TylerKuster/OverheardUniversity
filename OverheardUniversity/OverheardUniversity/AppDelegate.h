3dce4b6d423b8c6611250cbd795b2c7b4700816a
Tyler Kuster <tylerkuster@gmail.com>
2015-03-01 19:34:50 -0800

    created project

---
+++ OverheardUniversity/OverheardUniversity/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OverheardUniversity
+//
+//  Created by Tyler Kuster on 3/1/15.
+//  Copyright (c) 2015 Overheard University, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OverheardUniversity/OverheardUniversity/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OverheardUniversity
+//
+//  Created by Tyler Kuster on 3/1/15.
+//  Copyright (c) 2015 Overheard University, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
